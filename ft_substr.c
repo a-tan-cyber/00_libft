cf61b30ccad409202cccc4ecad810445c616c8a3
atan94 <amostan94@gmail.com>
2025-11-21 19:00:09 +0800

    done substr

--- ft_substr.c
+++ ft_substr.c
@@ -1,31 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/21 18:09:04 by amtan             #+#    #+#             */
-/*   Updated: 2025/11/21 18:58:21 by amtan            ###   ########.fr       */
+/*   Created: 2025/11/21 18:37:38 by amtan             #+#    #+#             */
+/*   Updated: 2025/11/21 18:55:09 by amtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <errno.h>
 #include <stdlib.h>
 
-char	*ft_strdup(const char *s)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*dup;
-	size_t	total;
+	char	*sub;
+	size_t	s_len;
+	size_t	sub_len;
 
-	total = ft_strlen(s) + 1;
-	dup = malloc(total);
-	if (!dup)
-	{
-		errno = ENOMEM;
+	if (!s)
 		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+	{
+		sub = malloc(1);
+		if (!sub)
+			return (NULL);
+		sub[0] = '\0';
+		return (sub);
 	}
-	ft_memcpy(dup, s, total);
-	return (dup);
+	if (len > s_len - start)
+		sub_len = s_len - start;
+	else
+		sub_len = len;
+	sub = malloc(sub_len + 1);
+	if (!sub)
+		return (NULL);
+	ft_memcpy(sub, s + start, sub_len);
+	sub[sub_len] = '\0';
+	return (sub);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 22:35:09 by amtan             #+#    #+#             */
-/*   Updated: 2025/11/21 17:56:51 by amtan            ###   ########.fr       */
+/*   Updated: 2025/11/21 18:36:41 by amtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
-char 	*ft_strdup(const char *s);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
