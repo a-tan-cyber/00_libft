f7a89ae9a52ab9d9482bef50055bda3566b9c445
atan94 <amostan94@gmail.com>
2025-11-23 00:47:32 +0800

    done ft_putnbr_fd

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: amtan <amtan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 22:35:09 by amtan             #+#    #+#             */
-/*   Updated: 2025/11/22 23:38:41 by amtan            ###   ########.fr       */
+/*   Updated: 2025/11/22 23:50:13 by amtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,5 +48,6 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
